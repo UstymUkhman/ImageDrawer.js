d4650f8d1ae120ee6c55ff482c64706de8ecdfc7
Ustym Ukhman <ustym.ukhman@gmail.com>
2016-02-16 02:31:34 +0100

    First Phase Init

--- script/imagedrawer.js
+++ script/imagedrawer.js
@@ -17,15 +17,40 @@
  * </div>
  * _________________________________________
  *
- * $(div#container).drawImge();
+ * $(div#container).drawImge({
+ *   duration: 20,            // Seconds it's take to draw the entire picture.
+ *   ||                        
+                              // Instead of specifying the duration on the whole animation,
+     {                        // it's also possible to set the duration of single drawing phases:
+ *     pencil: 5              // - seconds it's take to draw the picture by using only the black pencil
+ *   },
+ *
+ * });
  *
  */
+
 
 (function($) {
   $.fn.extend({
 
-    drawImge: function() {
+    drawImage: function(opts) {
 
+      // Number of Drawing Phases:
+      var PHASES = 4;
+
+      opts = opts || {
+        duration: 20
+      };
+
+      var phaseTiming = opts.duration / PHASES,
+          timing = {
+            pencil: (opts.duration.pencil || phaseTiming) + 's'
+          };
+
+      $(this)
+        .addClass('pencil')
+        .css({'animation-duration': timing.pencil})
+        .css({'-webkit-animation-duration': timing.pencil});
     }
 
   });
